917047ce85532e94644899edb51ecbe8cd3ada50
Asadbek Sayfiyev <333blackcat333@mail.ru>
2024-03-06 18:41:19 +0500

    Update debian_on_termux_10.sh

--- debian_on_termux_10.sh
+++ debian_on_termux_10.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
 # oldstable, stable, testing, unstable
-BRANCH=testing
+BRANCH=stable
 # base(258M), minbase(217M), buildd, fakechroot
 VAR=minbase
 # list_close_debian_mirrors.sh
@@ -11,38 +11,48 @@ set -e
 trap '[ $? -eq 0 ] && exit 0 || (echo; echo "termux-info:"; termux-info)' EXIT
 
 if [ ! -d ~/debian-$BRANCH ] ; then
-	ARCH=$(uname -m)
-	case $ARCH in
-		aarch64) ARCH=arm64 ;;
-		x86_64) ARCH=amd64 ;;
-		i686) ARCH=i386 ;;
-		armv7l) ARCH=armhf ;;
-		armv8l) apt-get -qq install getconf; if [ $(getconf LONG_BIT) -eq 64 ]; then ARCH=arm64; else ARCH=armhf; fi ;;
-		*) echo "Unsupported architecture $ARCH"; exit ;;
-	esac
-	apt-get -qq update
-	apt-get -qq dist-upgrade
-	apt-get -qq install debootstrap proot wget
-	debootstrap \
-		--variant=$VAR \
-		--exclude=systemd \
-		--arch=$ARCH \
-		$BRANCH \
-		~/debian-$BRANCH \
-		$REPO
+        ARCH=$(uname -m)
+        case $ARCH in
+                aarch64) ARCH=arm64 ;;
+                x86_64) ARCH=amd64 ;;
+                i686) ARCH=i386 ;;
+                armv7l) ARCH=armhf ;;
+                armv8l) apt-get -qq install getconf; if [ $(getconf LONG_BIT) -eq 64 ]; then ARCH=arm64; else ARCH=armhf; fi ;;
+                *) echo "Unsupported architecture $ARCH"; exit ;;
+        esac
+        apt-get -qq update
+        apt-get -qq dist-upgrade
+        pkg i debootstrap tsu wget
+        debootstrap \
+                --variant=$VAR \
+                --exclude=systemd \
+                --arch=$ARCH \
+                $BRANCH \
+                ~/debian-$BRANCH \
+                $REPO
 fi
 unset LD_PRELOAD
-proot \
-	-0 \
-	--link2symlink \
-	-r ~/debian-$BRANCH \
-	-w /root \
-	-b /dev/ \
-	-b /sys/ \
-	-b /proc/ \
-	-b /data/data/com.termux/files/home \
-	/usr/bin/env -i \
-	HOME=/root \
-	TERM="xterm-256color" \
-	PATH=/bin:/usr/bin:/sbin:/usr/sbin \
-	/bin/bash --login
+
+# The path of Ubuntu rootfs
+ROOTFS=~/debian-$BRANCH
+
+#sudo mount -o remount,dev,suid /data
+
+sudo mount --bind /dev $ROOTFS/dev
+sudo mount --bind /sys $ROOTFS/sys
+sudo mount --bind /proc $ROOTFS/proc
+sudo mount -t devpts devpts $ROOTFS/dev/pts
+
+# /dev/shm for Electron apps
+#sudo mount -t tmpfs -o size=256M tmpfs $ROOTFS/dev/shm
+
+# chroot (for networking link rootfs dir to `$PREFIX/var/lib/proot-distro/installed-rootfs/debian` proot & login: `proot-distro login debian` on Android 11+)
+sudo chroot $ROOTFS /bin/su - root
+
+sleep 2
+
+#sudo umount $ROOTFS/dev/shm
+sudo umount -lf $ROOTFS/dev/pts
+sudo umount -lf $ROOTFS/dev
+sudo umount -lf $ROOTFS/proc
+sudo umount -lf $ROOTFS/sys
